b5ed63106386e6ba65bb6dc1ad544dbab77c89e6
Adrian Round <adrian_round@telus.net>
2017-04-06 08:45:14 -0700

    Commit changes

--- Reflow Controller Interface/MainWindow.xaml.cs
+++ Reflow Controller Interface/MainWindow.xaml.cs
@@ -7,11 +7,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UsbHid;
+using UsbHid.USB.Classes.Messaging;
 
 namespace Reflow_Controller_Interface
 {
@@ -20,6 +23,8 @@ namespace Reflow_Controller_Interface
     /// </summary>
     public partial class MainWindow : Window
     {
+        public UsbHidDevice Device;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,10 +44,69 @@ namespace Reflow_Controller_Interface
             //toolStripStatusLabel1.Text = "Status: Not connected to port";*/
         }
 
-        public void InitialializeUSB()
+        private void InitialializeUSB(object sender, EventArgs e)
         {
-            
+            Device = new UsbHidDevice(0x04DB, 0x1234);
+            Device.OnConnected += DeviceOnConnected;
+            Device.OnDisConnected += DeviceOnDisConnected;
+            Device.DataReceived += DeviceDataReceived;
+            Device.Connect();
         }
+
+        private void DeviceDataReceived(byte[] data)
+        {
+            AppendText(ByteArrayToString(data));
+        }
+
+        private void AppendText(string p)
+        {
+            ThreadSafe(() => textBox1.AppendText(p + Environment.NewLine));
+        }
+
+        private void DeviceOnDisConnected()
+        {
+            ThreadSafe(() => checkBox1.Enabled = false);
+
+        }
+
+        private void DeviceOnConnected()
+        {
+            ThreadSafe(() => checkBox1.Enabled = true);
+        }
+
+        private void ThreadSafe(MethodInvoker method)
+        {
+            if (InvokeRequired)
+                Invoke(method);
+            else
+                method();
+        }
+
+        private static string ByteArrayToString(ICollection<byte> input)
+        {
+            var result = string.Empty;
+
+            if (input != null && input.Count > 0)
+            {
+                var isFirst = true;
+                foreach (var b in input)
+                {
+                    result += isFirst ? string.Empty : ",";
+                    result += b.ToString("X2");
+                    isFirst = false;
+                }
+            }
+            return result;
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            if (!Device.IsDeviceConnected) return;
+
+            var command = new CommandMessage(0x86, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 });
+            Device.SendMessage(command);
+        }
+        
 
     }
 }
